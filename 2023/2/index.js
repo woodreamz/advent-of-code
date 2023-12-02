1511cd7730444a2ea6db5ea99bcd553352f99095
Kévin Nové <knove@distech-controls.com>
2023-12-02 09:33:21 -0500

    day 2

--- 2023/2/index.js
+++ 2023/2/index.js
@@ -9,8 +9,37 @@ console.log('Advent Of Code - Day ' + DAY);
  * Part 1
  */
 const part1 = () => {
+  const maxCubes = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  };
+
   const input = fs.readFileSync('./2023/' + DAY + '/input.txt', 'utf-8');
-  return '';
+  const sum = input.split('\n').reduce((acc, line) => {
+    let isPossible = true;
+    const id = parseInt(line.substring(5, line.indexOf(':')));
+    const game = line.substring(line.indexOf(':') + 2, line.length);
+    const subsets = game.split('; ');
+    subsets.forEach((subset) => {
+      const set = subset.split(', ');
+      set.forEach((set) => {
+        const cubes = set.split(' ');
+        const num = parseInt(cubes[0]);
+        const color = cubes[1];
+
+        if (num > maxCubes[color]) {
+          isPossible = false;
+        }
+      });
+    });
+
+    if (isPossible) {
+      return acc + id;
+    }
+    return acc;
+  }, 0);
+  return sum;
 };
 
 /**
@@ -18,7 +47,30 @@ const part1 = () => {
  */
 const part2 = () => {
   const input = fs.readFileSync('./2023/' + DAY + '/input.txt', 'utf-8');
-  return '';
+  const sum = input.split('\n').reduce((acc, line) => {
+    const power = {
+      red: 0,
+      green: 0,
+      blue: 0,
+    };
+    const game = line.substring(line.indexOf(':') + 2, line.length);
+    const subsets = game.split('; ');
+    subsets.forEach((subset) => {
+      const set = subset.split(', ');
+      set.forEach((set) => {
+        const cubes = set.split(' ');
+        const num = parseInt(cubes[0]);
+        const color = cubes[1];
+
+        if (num > power[color]) {
+          power[color] = num;
+        }
+      });
+    });
+
+    return acc + power.red * power.green * power.blue;
+  }, 0);
+  return sum;
 };
 
 console.log('Part 1 = ', part1());
