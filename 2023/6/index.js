104e86c6ffce763e019c3d1e15a0400b50c5cd84
Kévin Nové <knove@distech-controls.com>
2023-12-06 16:15:54 -0500

    day6

--- 2023/6/index.js
+++ 2023/6/index.js
@@ -9,20 +9,53 @@ console.log('Advent Of Code - Day ' + DAY);
  * Part 1
  */
 const part1 = () => {
-  const lines = getLines(2023, DAY);
-  let sum = 0;
+  const lines = getLines(2023, DAY, 'input');
+  const times = lines[0]
+    .split(/[^\d]+/g)
+    .map((v) => parseInt(v))
+    .filter((v) => !isNaN(v));
+  const distances = lines[1]
+    .split(/[^\d]+/g)
+    .map((v) => parseInt(v))
+    .filter((v) => !isNaN(v));
 
-  return sum;
+  let result = 1;
+
+  times.forEach((time, index) => {
+    const distance = distances[index];
+    let ways = 0;
+
+    for (let i = 0; i <= time; i++) {
+      const temp = i * (time - i);
+
+      if (temp > distance) {
+        ways++;
+      }
+    }
+    result = result * ways;
+  });
+
+  return result;
 };
 
 /**
  * Part 2
  */
 const part2 = () => {
-  const lines = getLines(2023, DAY);
-  let sum = 0;
+  const lines = getLines(2023, DAY, 'input');
+  const time = parseInt(lines[0].replaceAll(' ', '').replace('Time:', ''));
+  const distance = parseInt(lines[1].replaceAll(' ', '').replace('Distance:', ''));
+  let ways = 0;
 
-  return sum;
+  for (let i = 0; i <= time; i++) {
+    const temp = i * (time - i);
+
+    if (temp > distance) {
+      ways++;
+    }
+  }
+
+  return ways;
 };
 
 console.log('Part 1 = ', part1());
