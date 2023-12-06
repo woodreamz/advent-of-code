7122895dce88633badb70cf46ea02fe8dbe08d0f
Kévin Nové <knove@distech-controls.com>
2023-12-06 09:13:12 -0500

    day5

--- 2023/5/index.js
+++ 2023/5/index.js
@@ -10,19 +10,118 @@ console.log('Advent Of Code - Day ' + DAY);
  */
 const part1 = () => {
   const lines = getLines(2023, DAY);
-  let sum = 0;
+  const seeds = lines[0]
+    .split(' ')
+    .map((seed) => parseInt(seed))
+    .filter((seed) => !isNaN(seed));
 
-  return sum;
+  const mapping = [];
+
+  lines.forEach((line, idx) => {
+    if (line === '' || idx === 0) {
+      return;
+    }
+
+    const matchMap = /([a-z]+)-to-([a-z]+)/g.exec(line);
+    const matchRange = /([0-9]+) ([0-9]+) ([0-9]+)/g.exec(line);
+    if (matchMap) {
+      const source = matchMap[1];
+      const target = matchMap[2];
+      mapping.push({
+        source,
+        target,
+        ranges: [],
+      });
+    } else if (matchRange) {
+      mapping[mapping.length - 1].ranges.push(matchRange[0].split(' ').map((v) => parseInt(v)));
+    }
+  });
+
+  const findTarget = (source, value) => {
+    const map = mapping.find((m) => m.source === source);
+    if (map) {
+      const match = map.ranges.find((ranges) => {
+        return value >= ranges[1] && value <= ranges[1] + ranges[2];
+      });
+
+      if (match) {
+        return findTarget(map.target, match[0] + value - match[1]);
+      }
+      return findTarget(map.target, value);
+    } else {
+      return value;
+    }
+  };
+
+  let location = null;
+  seeds.forEach((seed) => {
+    const r = findTarget('seed', seed);
+    location = location === null ? r : Math.min(location, r);
+  });
+
+  return location;
 };
 
 /**
  * Part 2
  */
 const part2 = () => {
-  const lines = getLines(2023, DAY);
-  let sum = 0;
+  const lines = getLines(2023, DAY, 'input');
+  const seeds = lines[0]
+    .split(' ')
+    .map((seed) => parseInt(seed))
+    .filter((seed) => !isNaN(seed));
 
-  return sum;
+  const mapping = [];
+
+  lines.forEach((line, idx) => {
+    if (line === '' || idx === 0) {
+      return;
+    }
+
+    const matchMap = /([a-z]+)-to-([a-z]+)/g.exec(line);
+    const matchRange = /([0-9]+) ([0-9]+) ([0-9]+)/g.exec(line);
+    if (matchMap) {
+      const source = matchMap[1];
+      const target = matchMap[2];
+      mapping.push({
+        source,
+        target,
+        ranges: [],
+      });
+    } else if (matchRange) {
+      mapping[mapping.length - 1].ranges.push(matchRange[0].split(' ').map((v) => parseInt(v)));
+    }
+  });
+
+  const findTarget = (source, value) => {
+    const map = mapping.find((m) => m.source === source);
+    if (map) {
+      const match = map.ranges.find((ranges) => {
+        return value >= ranges[1] && value <= ranges[1] + ranges[2];
+      });
+
+      if (match) {
+        return findTarget(map.target, match[0] + value - match[1]);
+      }
+      return findTarget(map.target, value);
+    } else {
+      return value;
+    }
+  };
+
+  let location = null;
+  seeds.forEach((seed, idx) => {
+    if (idx % 2 === 0) {
+      console.log('seed ' + idx, ' Start finding from ' + seed + ' to ' + (seed + seeds[idx + 1]));
+      for (let i = seed; i < seed + seeds[idx + 1]; i++) {
+        const result = findTarget('seed', i);
+        location = location === null ? result : Math.min(location, result);
+      }
+    }
+  });
+
+  return location;
 };
 
 console.log('Part 1 = ', part1());
