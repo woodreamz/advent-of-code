d7774631296baa89d1e558cc120828fceb49b18a
Kévin Nové <knove@distech-controls.com>
2023-12-08 09:52:51 -0500

    Generate futur days.

---
+++ 2023/13/index.js
@@ -0,0 +1,23 @@
+import fetchInput from '../../utils/fetchInput.js';
+import { getLines } from '../../utils/helpers.js';
+
+const DAY = 13;
+fetchInput(2023, DAY);
+console.log('Advent Of Code - Day ' + DAY);
+
+/**
+ * Part 1
+ */
+const part1 = () => {
+  const lines = getLines(2023, DAY);
+};
+
+/**
+ * Part 2
+ */
+const part2 = () => {
+  const lines = getLines(2023, DAY);
+};
+
+console.log('Part 1 = ', part1());
+console.log('Part 2 = ', part2());
