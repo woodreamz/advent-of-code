66d14f8e5953e965373b5efc0234b3dbc9841789
Kévin Nové <knove@distech-controls.com>
2023-12-15 08:19:15 -0500

    day 10 & 11

--- 2023/10/index.js
+++ 2023/10/index.js
@@ -10,6 +10,74 @@ console.log('Advent Of Code - Day ' + DAY);
  */
 const part1 = () => {
   const lines = getLines(2023, DAY);
+  let sum = 0;
+
+  let padding = ''.padStart(lines[0].length, '.');
+  lines.unshift(padding);
+  lines.push(padding);
+
+  let cells = [];
+  let start = [0, 0];
+  for (let y = 0; y < lines.length; y++) {
+    let line = '.' + lines[y] + '.';
+    cells.push(line.split(''));
+    if (line.indexOf('S') !== -1) {
+      start = [y, line.indexOf('S')];
+    }
+  }
+
+  let x = start[1];
+  let y = start[0];
+  let direction = 'X';
+  if (['F', '7', '|'].indexOf(cells[y - 1][x]) !== -1) {
+    direction = 'N';
+    y--;
+  } else if (['-', '7', 'J'].indexOf(cells[y][x + 1]) !== -1) {
+    direction = 'E';
+    x++;
+  } else if (['|', 'L', 'J'].indexOf(cells[y + 1][x]) !== -1) {
+    direction = 'S';
+    y++;
+  }
+  sum++;
+
+  //now loop until we reach back to S
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    let next = cells[y][x];
+    if (next == 'S') break;
+    let command = direction + next;
+    switch (command) {
+      case 'NF':
+      case 'E-':
+      case 'SL':
+        direction = 'E';
+        x++;
+        break;
+      case 'N|':
+      case 'EJ':
+      case 'WL':
+        direction = 'N';
+        y--;
+        break;
+      case 'N7':
+      case 'SJ':
+      case 'W-':
+        direction = 'W';
+        x--;
+        break;
+      case 'E7':
+      case 'S|':
+      case 'WF':
+        direction = 'S';
+        y++;
+        break;
+    }
+    sum++;
+  }
+  sum /= 2;
+
+  return sum;
 };
 
 /**
@@ -17,6 +85,127 @@ const part1 = () => {
  */
 const part2 = () => {
   const lines = getLines(2023, DAY);
+  let sum = 0;
+
+  let padding = ''.padStart(lines[0].length, '.');
+  lines.unshift(padding);
+  lines.push(padding);
+  let cells = [];
+  let iop = new Array(lines.length * 2)
+    .fill(0)
+    .map(() => new Array((lines[0].length + 2) * 2).fill('.'));
+  let start = [0, 0];
+  for (let y = 0; y < lines.length; y++) {
+    let line = '.' + lines[y] + '.';
+    cells.push(line.split(''));
+    if (line.indexOf('S') !== -1) {
+      start = [y, line.indexOf('S')];
+    }
+  }
+
+  let x = start[1];
+  let y = start[0];
+  let a = x * 2;
+  let b = y * 2;
+  iop[b][a] = '#';
+  let direction = 'X';
+  if (['F', '7', '|'].indexOf(cells[y - 1][x]) !== -1) {
+    direction = 'N';
+    y--;
+    iop[b - 1][a] = '#';
+  } else if (['-', '7', 'J'].indexOf(cells[y][x + 1]) !== -1) {
+    direction = 'E';
+    iop[b][a + 1] = '#';
+    x++;
+  } else if (['|', 'L', 'J'].indexOf(cells[y + 1][x]) !== -1) {
+    direction = 'S';
+    iop[b + 1][a] = '#';
+    y++;
+  }
+
+  //now loop until we reach back to S
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    a = x * 2;
+    b = y * 2;
+    iop[b][a] = '#';
+    let next = cells[y][x];
+    if (next == 'S') break;
+    let command = direction + next;
+    switch (command) {
+      case 'NF':
+      case 'E-':
+      case 'SL':
+        direction = 'E';
+        x++;
+        iop[b][a + 1] = '#';
+        break;
+      case 'N|':
+      case 'EJ':
+      case 'WL':
+        direction = 'N';
+        y--;
+        iop[b - 1][a] = '#';
+        break;
+      case 'N7':
+      case 'SJ':
+      case 'W-':
+        direction = 'W';
+        x--;
+        iop[b][a - 1] = '#';
+        break;
+      case 'E7':
+      case 'S|':
+      case 'WF':
+        direction = 'S';
+        y++;
+        iop[b + 1][a] = '#';
+        break;
+    }
+  }
+
+  let bmax = iop.length - 1;
+  let amax = iop[0].length - 1;
+
+  const fillmap = (b, a) => {
+    let stack = [{ b: b, a: a }];
+    const directions = [
+      [0, 1],
+      [0, -1],
+      [1, 0],
+      [-1, 0],
+    ];
+    while (stack.length > 0) {
+      let current = stack.pop();
+      for (let i = 0; i < 4; i++) {
+        let child = { b: current.b + directions[i][0], a: current.a + directions[i][1] };
+        if (
+          child.a >= 0 &&
+          child.a <= amax &&
+          child.b >= 0 &&
+          child.b <= bmax &&
+          iop[child.b][child.a] == '.'
+        ) {
+          iop[child.b][child.a] = 'O';
+          stack.push(child);
+        }
+      }
+    }
+  };
+
+  fillmap(0, 0);
+
+  //find inners
+  for (let b = 0; b < bmax; b += 2) {
+    for (let a = 0; a < amax; a += 2) {
+      if (iop[b][a] == '.') {
+        sum++;
+        iop[b][a] = 'I';
+      }
+    }
+  }
+
+  return sum;
 };
 
 console.log('Part 1 = ', part1());
--- 2023/11/index.js
+++ 2023/11/index.js
@@ -10,6 +10,44 @@ console.log('Advent Of Code - Day ' + DAY);
  */
 const part1 = () => {
   const lines = getLines(2023, DAY);
+  let sum = 0;
+  let expanded = lines.reduce((acc, line) => {
+    const points = line.split('');
+    if (points.includes('#') == false) {
+      acc.push(points);
+    }
+    acc.push(points);
+    return acc;
+  }, []);
+
+  let flipped = expanded[0].map((col, idx) => expanded.map((row, idx2) => expanded[idx2][idx]));
+
+  expanded = [];
+  flipped.forEach((points) => {
+    if (points.includes('#') == false) {
+      expanded.push(points);
+    }
+    expanded.push(points);
+  });
+
+  flipped = expanded[0].map((col, c) => expanded.map((row, r) => expanded[r][c]));
+
+  let galaxies = [];
+  for (let i = 0; i < flipped.length; i++) {
+    for (let j = 0; j < flipped[0].length; j++) {
+      if (flipped[i][j] == '#') {
+        galaxies.push([i, j]);
+      }
+    }
+  }
+
+  for (let i = 0; i < galaxies.length - 1; i++) {
+    for (let j = i + 1; j < galaxies.length; j++) {
+      sum += Math.abs(galaxies[i][0] - galaxies[j][0]) + Math.abs(galaxies[i][1] - galaxies[j][1]);
+    }
+  }
+
+  return sum;
 };
 
 /**
@@ -17,6 +55,57 @@ const part1 = () => {
  */
 const part2 = () => {
   const lines = getLines(2023, DAY);
+  let sum = 0;
+
+  const expansion = 1000000 - 1;
+  const grid = lines.map((line) => line.split(''));
+  let expCols = [];
+  let expRows = [];
+  let cache = [];
+  let galaxies = [];
+  for (let i = 0; i < grid.length; i++) {
+    if (grid[i].includes('#') == false) {
+      expRows.push(i);
+    }
+    for (let j = 0; j < grid[0].length; j++) {
+      if (i == 0) cache[j] = true;
+      if (grid[i][j] == '#') {
+        galaxies.push([i, j]);
+        cache[j] = false;
+      }
+    }
+  }
+  cache.forEach((value, idc) => {
+    if (value == true) {
+      expCols.push(idc);
+    }
+  });
+
+  for (let i = 0; i < galaxies.length - 1; i++) {
+    for (let j = i + 1; j < galaxies.length; j++) {
+      let length =
+        Math.abs(galaxies[i][0] - galaxies[j][0]) + Math.abs(galaxies[i][1] - galaxies[j][1]);
+      expCols.forEach((col) => {
+        if (
+          Math.min(galaxies[i][1], galaxies[j][1]) < col &&
+          Math.max(galaxies[i][1], galaxies[j][1]) > col
+        ) {
+          length += expansion;
+        }
+      });
+      expRows.forEach((row) => {
+        if (
+          Math.min(galaxies[i][0], galaxies[j][0]) < row &&
+          Math.max(galaxies[i][0], galaxies[j][0]) > row
+        ) {
+          length += expansion;
+        }
+      });
+      sum += length;
+    }
+  }
+
+  return sum;
 };
 
 console.log('Part 1 = ', part1());
